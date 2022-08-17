f8c0fc79b39998ebfb8f4531cda34cbb26b48973
Animeshpandey202 <98416957+Animeshpandey202@users.noreply.github.com>
2022-08-18 01:36:32 +0530

    initial commit

---
+++ Untitled-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	while(t>0){
+	    
+	    int n,x;
+	    cin>>n>>x;
+	    cout<<2*n-x+1;
+	    
+	    
+	    
+	    t--;
+	}
+	return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+nt area(int leftr,int rightr,int sum,int a,int b){
+//     int flag=0;
+//     while(flag==0){
+        
+//         int leftr=rightr*a;
+//         if(leftr>0){
+//             sum+=3.14*leftr*leftr;
+//         }
+//         else{
+//             flag=1;
+//             break;
+//         }
+       
+        
+//         int rightr=leftr/b;
+//         if(rightr>0){
+//             sum+=3.14*rightr*rightr;
+//         }
+//         else{
+//             flag=1;
+//             break;
+//         }
+//        return sum;
+//     }
+    
+    
+    
+// }
